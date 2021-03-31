52c4979bf17ec9697ad548606b3541ef984c3c19
Pengkuh Dwi Septiandi <pengkuh.dst@gmail.com>
2021-03-31 12:20:07 +0700

    tambah button open ContainerActivity

--- app/src/main/java/io/github/pengdst/praktikumbp/HomeActivity.kt
+++ app/src/main/java/io/github/pengdst/praktikumbp/HomeActivity.kt
@@ -10,6 +10,7 @@ class HomeActivity : AppCompatActivity() {
 
     lateinit var buttonClose: Button
     lateinit var buttonImplicit: Button
+    lateinit var buttonOpenContainer: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +18,7 @@ class HomeActivity : AppCompatActivity() {
 
         buttonClose = findViewById(R.id.btn_close)
         buttonImplicit = findViewById(R.id.btn_implicit)
+        buttonOpenContainer = findViewById(R.id.btn_container)
 
         buttonImplicit.setOnClickListener {
             val intentOpenBrowser = Intent(Intent.ACTION_VIEW, Uri.parse("http://youtube.com"))
@@ -24,6 +26,10 @@ class HomeActivity : AppCompatActivity() {
         }
         buttonClose.setOnClickListener {
             finish()
+        }
+        buttonOpenContainer.setOnClickListener {
+            val intentContainer = Intent(this, ContainerActivity::class.java)
+            startActivity(intentContainer)
         }
     }
 }
